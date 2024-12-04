13209b7be32aba929d42fa84ba61b82f04e56321
Cristian Goina <goinac@hhmi.org>
2024-12-04 10:38:53 -0500

    display time needed to finish grad score for a mask

--- colormipsearch-tools/src/main/java/org/janelia/colormipsearch/cmd/CalculateGradientScoresCmd.java
+++ colormipsearch-tools/src/main/java/org/janelia/colormipsearch/cmd/CalculateGradientScoresCmd.java
@@ -277,7 +277,7 @@ class CalculateGradientScoresCmd extends AbstractCmd {
                     (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / _1M + 1, // round up
                     (Runtime.getRuntime().totalMemory() / _1M));
         }
-        LOG.info("{} completed grad score for mask {}", processingContext, maskId);
+        LOG.info("{} completed grad score for mask {} in {} secs", processingContext, maskId, (System.currentTimeMillis()-startProcessingMask) / 1000.);
         System.gc(); // explicitly garbage collect
         return nupdates.get();
     }
