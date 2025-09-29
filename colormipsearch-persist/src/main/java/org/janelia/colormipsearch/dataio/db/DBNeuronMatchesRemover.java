a9b7f98bbcc813146fe694baef241ab066f8779f
Cristian Goina <goinac@janelia.hhmi.org>
2025-09-29 14:36:59 -0400

    logging

--- colormipsearch-persist/src/main/java/org/janelia/colormipsearch/dataio/db/DBNeuronMatchesRemover.java
+++ colormipsearch-persist/src/main/java/org/janelia/colormipsearch/dataio/db/DBNeuronMatchesRemover.java
@@ -1,17 +1,18 @@
 package org.janelia.colormipsearch.dataio.db;
 
 import java.util.List;
-import java.util.function.Function;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.janelia.colormipsearch.dao.NeuronMatchesDao;
 import org.janelia.colormipsearch.dataio.NeuronMatchesRemover;
-import org.janelia.colormipsearch.dataio.NeuronMatchesWriter;
 import org.janelia.colormipsearch.model.AbstractMatchEntity;
 import org.janelia.colormipsearch.model.AbstractNeuronEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DBNeuronMatchesRemover<R extends AbstractMatchEntity<? extends AbstractNeuronEntity, ? extends AbstractNeuronEntity>>
         implements NeuronMatchesRemover<R> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DBNeuronMatchesRemover.class);
 
     private final NeuronMatchesDao<R> neuronMatchesDao;
     private final boolean archiveMatchesOnDelete;
@@ -24,8 +25,10 @@ public class DBNeuronMatchesRemover<R extends AbstractMatchEntity<? extends Abst
     @Override
     public long delete(List<R> matches) {
         if (archiveMatchesOnDelete) {
+            LOG.info("Archiving {} matches", matches.size());
             return neuronMatchesDao.archiveMatches(matches);
         } else {
+            LOG.info("Removing {} matches", matches.size());
             return neuronMatchesDao.deleteMatches(matches);
         }
     }
