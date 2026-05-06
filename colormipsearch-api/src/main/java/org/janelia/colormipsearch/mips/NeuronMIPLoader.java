5e0a9dd2d5f6cea45ffda90614364303817afc03
Cristian Goina <goinac@janelia.hhmi.org>
2026-05-06 16:20:11 -0400

    removed unused imports

--- colormipsearch-api/src/main/java/org/janelia/colormipsearch/mips/NeuronMIPLoader.java
+++ colormipsearch-api/src/main/java/org/janelia/colormipsearch/mips/NeuronMIPLoader.java
@@ -1,11 +1,7 @@
 package org.janelia.colormipsearch.mips;
 
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.type.Type;
-import net.imglib2.type.numeric.IntegerType;
 import org.janelia.colormipsearch.model.AbstractNeuronEntity;
 import org.janelia.colormipsearch.model.ComputeFileType;
-import org.janelia.colormipsearch.model.FileData;
 
 /**
  * This is used for loading
